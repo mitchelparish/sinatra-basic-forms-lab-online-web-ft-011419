99bb906f6f6a78de404cedad1a9810336472085e
Mitchel Parish <mitchelparish@gmail.com>
2019-02-27 18:29:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,11 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  # post '/puppy' do
-  #
-  #
-  #   erb: display_puppy
-  # end
-
-
+  post '/puppy' do
+    @new_puppy = Puppy.new(params["name"], params["breed"], params["age"])
+    erb :display_puppy
+  end
 end
